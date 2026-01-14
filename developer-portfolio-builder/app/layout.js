0206e0c46d00d78791aa84a49c57f3d631143097
Vedant Nilakh <vedantnilakh@gmail.com>
2026-01-14 14:07:13 +0530

    Add Clerk authentication and auth routes

--- developer-portfolio-builder/app/layout.js
+++ developer-portfolio-builder/app/layout.js
@@ -1,5 +1,13 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +26,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div>
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
